bb31ff16dd1e6d34632ea4422a3fed132504d539
BaranovOleksandr <baranovoc@gmail.com>
2020-01-25 00:00:29 +0200

    Counter

---
+++ src/scripts/index.js
@@ -0,0 +1,49 @@
+import { store } from './counterState';
+
+const counterValueNode = document.getElementById('counter-value');
+const counterDecrementButtonNode = document.getElementById('counter-decrement-button');
+const counterIncrementButtonNode = document.getElementById('counter-increment-button');
+
+function createCounterIncrementAction() {
+    return {
+        type: 'INCREMENT',
+    };
+}
+
+function createCounterDecrementAction() {
+    return {
+        type: 'DECREMENT',
+    };
+}
+
+function counterReducer(state = 0, action) {
+
+    if (action.type === 'INCREMENT') {
+        return state + 1;
+    }
+
+    if (action.type === 'DECREMENT') {
+        return state - 1;
+    }
+
+    return state;
+}
+
+store = window.Redux.createStore(counterReducer);
+
+store.subscribe(() => {
+    const state = store.getState();
+
+    console.log(`Something has been changed - ${state}`);
+
+    counterValueNode.innerText = state;
+});
+
+counterIncrementButtonNode.addEventListener('click',() =>{
+    store.dispatch(createCounterIncrementAction());
+});
+
+counterDecrementButtonNode.addEventListener('click',() =>{
+    store.dispatch(createCounterDecrementAction());
+});
+
